ca8cea631be3442820137f84cbe2d65d976253cf
João Martins Costa <jpmcosta@gmail.com>
2020-05-29 23:11:09 +0100

    Test project for RecyclerView issue

--- app/src/main/java/com/jpmcosta/androidtestproject/activity/MainActivity.kt
+++ app/src/main/java/com/jpmcosta/androidtestproject/activity/MainActivity.kt
@@ -2,13 +2,65 @@ package com.jpmcosta.androidtestproject.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.jpmcosta.androidtestproject.R
+import com.jpmcosta.androidtestproject.adapter.NumberAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        private const val COUNT = 20
+    }
+
+    @BindView(R.id.fab)
+    lateinit var fab: FloatingActionButton
+
+    @BindView(R.id.fab_reset)
+    lateinit var fabReset: FloatingActionButton
+
+    @BindView(android.R.id.list)
+    lateinit var recyclerView: RecyclerView
+
+    private var numberAdapter = NumberAdapter(COUNT)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupContentView()
+    }
+
+    private fun setupContentView() {
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
+        setupFabs()
+
+        setupRecyclerView()
+    }
+
+    private fun setupFabs() {
+        fab.setOnClickListener {
+            numberAdapter.count--
+        }
+
+        fabReset.setOnClickListener {
+            numberAdapter.count = COUNT
+            (recyclerView.layoutManager as? LinearLayoutManager)?.scrollToPositionWithOffset(COUNT - 1, 0)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.run {
+            (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            adapter = numberAdapter
+        }
     }
 }
--- app/src/main/java/com/jpmcosta/androidtestproject/service/NotificationListener.kt
+++
@@ -1,5 +0,0 @@
-package com.jpmcosta.androidtestproject.service
-
-import android.service.notification.NotificationListenerService
-
-class NotificationListener : NotificationListenerService()
