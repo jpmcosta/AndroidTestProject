43793368a74f97fff17818ebb5455594910b3002
João Martins Costa <jpmcosta@gmail.com>
2020-06-29 21:19:50 +0100

    Test for API 21 ActivityCompat.recreate() issue

--- app/src/main/java/com/jpmcosta/androidtestproject/activity/MainActivity.kt
+++ app/src/main/java/com/jpmcosta/androidtestproject/activity/MainActivity.kt
@@ -1,66 +1,38 @@
 package com.jpmcosta.androidtestproject.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SimpleItemAnimator
-import butterknife.BindView
-import butterknife.ButterKnife
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.core.app.ActivityCompat
 import com.jpmcosta.androidtestproject.R
-import com.jpmcosta.androidtestproject.adapter.NumberAdapter
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
 
-        private const val COUNT = 20
+        private const val TAG: String = "recreate"
+
+        private var recreateSeqNum: Int = 0
     }
-
-    @BindView(R.id.fab)
-    lateinit var fab: FloatingActionButton
-
-    @BindView(R.id.fab_reset)
-    lateinit var fabReset: FloatingActionButton
-
-    @BindView(android.R.id.list)
-    lateinit var recyclerView: RecyclerView
-
-    private var numberAdapter = NumberAdapter(COUNT)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupContentView()
-    }
-
-    private fun setupContentView() {
         setContentView(R.layout.activity_main)
-        ButterKnife.bind(this)
-
-        setupFabs()
-
-        setupRecyclerView()
     }
 
-    private fun setupFabs() {
-        fab.setOnClickListener {
-            numberAdapter.count--
-        }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume() $this")
 
-        fabReset.setOnClickListener {
-            numberAdapter.count = COUNT
-            (recyclerView.layoutManager as? LinearLayoutManager)?.scrollToPositionWithOffset(COUNT - 1, 0)
-        }
-    }
-
-    private fun setupRecyclerView() {
-        recyclerView.run {
-            (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
-            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
-            adapter = numberAdapter
+        val result = recreateSeqNum++ % 4
+        if (result == 0) {
+            Log.i(TAG, "ActivityCompat.recreate()")
+            ActivityCompat.recreate(this)
+        } else if (result == 2) {
+            Log.i(TAG, "recreate()")
+            recreate()
         }
     }
 }
