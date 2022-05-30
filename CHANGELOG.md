d79f65e2f5cd6ed977727205da337096332e5ef3
João Martins Costa <jpmcosta@gmail.com>
2022-05-30 16:13:38 +0100

    Create new project

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-(2020-12-12) Test project for https://issuetracker.google.com/issues/175417675
-
-(2020-06-29) Test project for https://issuetracker.google.com/issues/160122826
-
-(2020-05-29) Test project for https://issuetracker.google.com/issues/157719649
---
+++ app/src/main/java/com/jpmcosta/androidtestproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jpmcosta.androidtestproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jpmcosta.androidtestproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
