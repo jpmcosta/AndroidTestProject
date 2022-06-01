93490ed30ace4ed95e9857b8e488c455cb7725bb
João Martins Costa <jpmcosta@gmail.com>
2022-06-01 11:57:57 +0100

    Test project for ImageView bounds

--- app/src/main/java/com/jpmcosta/androidtestproject/MainActivity.kt
+++ app/src/main/java/com/jpmcosta/androidtestproject/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.jpmcosta.androidtestproject
 
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.jpmcosta.androidtestproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupIcon(root = binding.root, icon = binding.iconIssue)
+        setupIcon(root = binding.root, icon = binding.iconFixed)
+    }
+
+    private fun setupIcon(root: MotionLayout, icon: ImageView) {
+        root.setPaddingConstraints(viewId = icon.id, padding = 0)
+
+        root.post {
+            val padding = resources.getDimensionPixelSize(R.dimen.padding_icon)
+            root.setPaddingConstraints(viewId = icon.id, padding)
+
+            icon.setImageResource(R.drawable.ic_android_128)
+        }
+    }
+
+    private fun MotionLayout.setPaddingConstraints(@IdRes viewId: Int, padding: Int) {
+        getConstraintSet(R.id.start).run {
+            setIntValue(viewId, "Padding", padding)
+        }
+        getConstraintSet(R.id.end).run {
+            setIntValue(viewId, "Padding", padding)
+        }
     }
 }
---
+++ app/src/main/java/com/jpmcosta/androidtestproject/view/PaddingImageViewFixed.kt
@@ -0,0 +1,29 @@
+package com.jpmcosta.androidtestproject.view
+
+import android.content.Context
+import android.util.AttributeSet
+
+class PaddingImageViewFixed : PaddingImageView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+    private var hasFrame: Boolean = true
+
+    override fun setFrame(l: Int, t: Int, r: Int, b: Int): Boolean {
+        hasFrame = true
+        return super.setFrame(l, t, r, b)
+    }
+
+    override fun invalidateOutline() {
+        super.invalidateOutline()
+
+        if (drawable != null && hasFrame) { // Based on ImageView.configureBounds() condition.
+            setFrame(left, top, right, bottom)
+        }
+    }
+}
